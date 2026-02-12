2a27bc0203d2c938d949e0ed8ba993a456ad7e23
HPOINT <r>
2026-02-12 18:29:27 +0300

    заготовки структур под данные и перенос классов в папки

---
+++ LogsGraph/LogsGraph/DataStorage/FileFormat.cs
@@ -0,0 +1,18 @@
+﻿//описание формата для парсинга файла
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogsGraph.DataStorage
+{
+    public class FileFormat
+    {
+        public string Name;//имя для данного формата
+        public int IgnoreRows = 0;//сколько первых строк в файле игнорировать
+        public bool MultiX = false; //для каждого графика свой столбец для X
+        public string Separate = ";"; //разделитель значений
+        public string PointSimbol=".";//разделите для числа с плавающей точкой
+
+        public List<string> CustomMarkers = new List<string>(); //маркеры столбцов на случай если они не описаны в файле
+    }
+}
---
+++ LogsGraph/LogsGraph/DataStorage/SingleData.cs
@@ -0,0 +1,20 @@
+﻿//данные для одного графика
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogsGraph
+{
+    public class SinglePoint
+    {
+        public double X;
+        public double Y;
+    }
+    public class SingleData
+    {
+        public string Name;//имя для отображения
+        public string Marker;//маркер для парсинга
+
+        List<SinglePoint> Points;
+    }
+}
