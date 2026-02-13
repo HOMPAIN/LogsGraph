1e4fb94cf4d9e4dd841bf2cc6a7763275b21df9c
HPOINT <r>
2026-02-13 18:20:09 +0300

    подготовка формы парсинга файлов

---
+++ LogsGraph/LogsGraph/GUI/DataParser.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LogsGraph.GUI
+{
+    /// <summary>
+    /// Логика взаимодействия для DataParser.xaml
+    /// </summary>
+    public partial class DataParser : UserControl
+    {
+        public DataParser()
+        {
+            InitializeComponent();
+        }
+    }
+}
